c1c132177c8a6040e9835e71cb9ca06051d14918
MandyMY <wangmychn@gmail.com>
2022-06-22 21:51:17 -0400

    Fix Conflict - Recover Yiyang's files

--- worflow_airflow/workflow.py
+++ worflow_airflow/workflow.py
@@ -1,7 +1,7 @@
 # -*- codingL utf-8 -*-
 # @Time : 5/12/22 6:18 PM
 # @Author : YIYANG BIAN
-# @File : workflow.py
+# @File : worflow_airflow.py
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'byy',
     'depends_on_past': True,
-    'start_date': datetime(2022, 5, 19),
+    'start_date': datetime(2022, 6, 2),
     'email': ['byy981109@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -29,6 +29,29 @@ dag = DAG(
 )
 
 task1 = BashOperator(
+    task_id='source_infomation',
+    bash_command="cd /Users/bianyiyang/airflow/dags/CRUX/to_cards/ && python3 generate_source.py --name 'Jacob L. Jones'\
+                  --affiliation 'North Carolina State University'\
+                  --positions 'Kobe Distinguished Professor, Materials Science and Engineering'\
+                  --positions 'Director, Science and Technologies for Phosphorus Sustainability (STEPS) Center'\
+                  --positions 'Director, Research Triangle Nanotechnology Network'\
+                  --address '3072B Engineering Building I, Raleigh, NC'\
+                  --phone 919-515-4557\
+                  --email jacobjones@ncsu.edu && python3 generate_source.py --name 'Mauro Sardela'\
+                  --affiliation 'University of Illinois at Urbana-Champaign'\
+                  --positions 'Director, Central Research Facilities, Materials Research Laboratory'\
+                  --address '104 S. Goodwin Avenue – Urbana IL 61801'\
+                  --website www.mrl.illinois.edu\
+                  --phone 217-244-0547\
+                  --email sardela@illinois.edu\
+                  --office '#SC2014'",
+    # python_callable=datacards,
+    retries=3,
+    dag=dag
+)
+
+
+task2 = BashOperator(
     task_id='data_cards',
     bash_command="cd /Users/bianyiyang/airflow/dags/CRUX/to_cards/ && python3 datacard.py -file '../content/data/xrdml/NASA/BZnZr - BIn - BSc - PT/2.5Bi(Zn0.5Zr0.5)O3 - 5BiInO3 - 32.5BiScO3 - 60PbTiO3_1100C.xrdml'",
     # python_callable=datacards,
@@ -36,7 +59,7 @@ task1 = BashOperator(
     dag=dag
 )
 
-task2 = BashOperator(
+task3 = BashOperator(
     task_id='task_cards',
     bash_command="python3 /Users/bianyiyang/airflow/dags/CRUX/to_cards/taskcard.py peak_finding --input_format xrdml\
                                  --output_format txt\
@@ -47,7 +70,7 @@ task2 = BashOperator(
     dag=dag
 )
 
-task3 = BashOperator(
+task4 = BashOperator(
     task_id='model_cards',
     bash_command="cd /Users/bianyiyang/airflow/dags/CRUX/to_cards/ && python3 modelcard.py --modelName scipy.signal.find_peaks\
                      --affiliation 'The SciPy community'\
@@ -61,13 +84,26 @@ task3 = BashOperator(
                      --inputFormat nparray\
                      --outputFormat nparray\
                      --inputParameters x height threshold distance prominence width wlen rel_height plateau_size\
-                     --outputParameters peaks properties",
+                     --outputParameters peaks properties && python3 modelcard.py --modelName peakutils.peak.index\
+                     --username 'Lucas Hermann Negri'\
+                     --contactInfo 'lucashnegri@gmail.com'\
+                     --license 'MIT license'\
+                     --modelDate 01/23/2020\
+                     --modelVersion 1.3.3\
+                     --modelType 'Peak detection routine.'\
+                     --paperOrResource https://peakutils.readthedocs.io/en/latest/reference.html#module-peakutils.peak\
+                     --modelLocation https://bitbucket.org/lucashnegri/peakutils\
+                     --dependencies numpy scipy\
+                     --inputFormat nparray\
+                     --outputFormat nparray\
+                     --inputParameters y thres min_dist thres_abs\
+                     --outputParameters 'Array containing the numeric indexes of the peaks that were detected.'",
     # python_callable=x2,
     retries=3,
     dag=dag
 )
 
-task4 = BashOperator(
+task5 = BashOperator(
     task_id='scipy_model_cards',
     bash_command="cd /Users/bianyiyang/airflow/dags/CRUX/to_cards/ && python3 modelcard.py --modelName pf_scipy_dist200\
                      --modelLocation ../content/model/peakfinding/pf_scipy_dist200.py\
@@ -78,12 +114,12 @@ task4 = BashOperator(
                      --outputParameters peaklist\
                      --hyperParameters min_dist=200\
                      --taskName peak_finding",
-    # python_callable=x2,
     retries=3,
     dag=dag
 )
 
-task5 = BashOperator(
+
+task6 = BashOperator(
     task_id='test_cards',
     bash_command="cd /Users/bianyiyang/airflow/dags/CRUX/to_cards/ && python3 testcard.py peak_finding",
     # python_callable=x2,
@@ -91,12 +127,11 @@ task5 = BashOperator(
     dag=dag
 )
 
-task6 = BashOperator(
+task7 = BashOperator(
     task_id='performance_cards',
     bash_command="python3 /Users/bianyiyang/airflow/dags/CRUX/to_cards/performance.py 0.01",
     retries=3,
     dag=dag
 )
 
-task1 >> task2 >> task3 >> task4 >> task5 >> task6
-
+[task1, task2, task3] >> task4 >> task5 >> task6 >> task7
---
+++ workflow_pegasus/pegasus_workflow.py
@@ -0,0 +1,111 @@
+# -*- codingL utf-8 -*-
+# @Time : 6/8/22 10:05 PM
+# @Author : YIYANG BIAN
+# @File : generate_source.py
+
+import logging
+from pathlib import Path
+from pegasus.api import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+# --- Specify Input Files -----------
+
+
+# --- Specify Executables -----------
+generate_sourcecard = Transformation(
+    name="generate_source",
+    site="local",
+    pfn="/Users/bianyiyang/airflow/dags/CRUX/workflow_pegasus/generate_source.py",
+    is_stageable=True,
+)
+
+generate_datacard = Transformation(
+    name="generate_datacard",
+    site="local",
+    pfn="/Users/bianyiyang/airflow/dags/CRUX/workflow_pegasus/generate_datacard.py",
+    is_stageable=True,
+)
+
+generate_taskcard = Transformation(
+    name="generate_taskcard",
+    site="local",
+    pfn=Path(".").resolve() / "CRUX/to_cards/taskcard.py",
+    is_stageable=True,
+)
+
+generate_modelcard = Transformation(
+    name="modelcard.py",
+    site="local",
+    pfn=Path(".").resolve() / "CRUX/to_cards/modelcard.py",
+    is_stageable=True,
+)
+
+generate_testcard = Transformation(
+    name="testcard.py",
+    site="local",
+    pfn=Path(".").resolve() / "CRUX/to_cards/testcard.py",
+    is_stageable=True,
+)
+
+generate_performancecard = Transformation(
+    name="performance.py",
+    site="local",
+    pfn=Path(".").resolve() / "CRUX/to_cards/performance.py",
+    is_stageable=True,
+)
+
+generate_ranking = Transformation(
+    name="ranking.py",
+    site="local",
+    pfn=Path(".").resolve() / "CRUX/to_cards/ranking.py",
+    is_stageable=True,
+)
+
+
+tc = TransformationCatalog().add_transformations(generate_sourcecard,generate_datacard,generate_taskcard,generate_modelcard,generate_testcard,generate_performancecard,generate_ranking)
+
+# --- Build Workflow ---------------
+wf = Workflow("peakfinding-workflow")
+
+fa = File("f.a")
+fb = File("f.b")
+peakfinding_job1 = Job(generate_sourcecard).add_args("-a","generate_soucrcecard","-T","3","-i",fa,"-o",fb).add_inputs(fa).add_outputs(fb)
+
+fc = File("f.c")
+peakfinding_job2 = Job(generate_datacard).add_args("-a","generate_datacard","-T","3","-i",fb,"-o",fc).add_inputs(fb).add_outputs(fc)
+
+fd = File("f.d")
+peakfinding_job3 = Job(generate_taskcard).add_args("-a","generate_taskcard","-T","3","-i",fc,"-o",fd).add_inputs(fc).add_outputs(fd)
+
+fe = File("f.e")
+peakfinding_job4 = Job(generate_modelcard).add_args("-a","generate_modelcard","-T","3","-i",fd,"-o",fe).add_inputs(fd).add_outputs(fe)
+
+ff = File("f.f")
+peakfinding_job5 = Job(generate_testcard).add_args("-a","generate_testcard","-T","3","-i",fe,"-o",ff).add_inputs(fe).add_outputs(ff)
+
+fg = File("f.g")
+peakfinding_job6 = Job(generate_performancecard).add_args("-a","generate_performancecard","-T","3","-i",ff,"-o",fg).add_inputs(ff).add_outputs(fg)
+
+fh = File("f.h")
+peakfinding_job7 = Job(generate_ranking).add_args("-a","generate_rankingcard","-T","3","-i",fg,"-o",fh).add_inputs(fg).add_outputs(fh)
+
+wf.add_transformation_catalog(tc)
+wf.add_jobs(peakfinding_job1, peakfinding_job2,peakfinding_job3,peakfinding_job4,peakfinding_job5,peakfinding_job6,peakfinding_job7)
+
+try:
+    wf.write()
+    wf.graph(include_files=True, label="xform-id", output="graph.png")
+except PegasusClientError as e:
+    print(e)
+
+# view rendered workflow
+from IPython.display import Image
+Image(filename='graph.png')
+
+# Run the Workflow
+try:
+    wf.plan(submit = True)\
+        .wait()
+except PegasusClientError as e:
+    print(e)
