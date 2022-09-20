ca157cfd4e42e1de2e7ceeaa071db28664475ca6
MandyMY <wangmychn@gmail.com>
2022-09-20 05:41:33 -0400

    GNN input - Add Feature to Models and Data

--- gnn/input/csv2txt.py
+++ gnn/input/csv2txt.py
@@ -6,6 +6,8 @@ from bson.objectid import ObjectId
 from gensim.models import Word2Vec
 import numpy as np
 import ast
+from statistics import mean
+import xmltodict
 
 sys.path.append("ast2vec")
 import ast2vec as a2v
@@ -31,7 +33,7 @@ def perform(a, b, c):
 
 
 # Get the average vector for a node's information.
-def get_vec_mean(info, vec_size):
+def get_vec_mean(info, vec_size, w2v_model):
     vec_sum = np.zeros(vec_size)
     n = 0
     for term in info:
@@ -53,28 +55,49 @@ def ast_to_vec(f):
     return x.detach().numpy().tolist()
 
 
-# def break_list(minfo, in_list):
-#     for i in in_list:
-#         minfo.append(i)
-#
-#     return minfo
-#
-#
+def break_list(minfo, in_list):
+    for i in in_list:
+        minfo.append(i)
+
+    return minfo
+
+
+# Get x and y from raw data
+def detect_x_y(input):
+    f = open(input, mode='r', encoding='utf-8')
+    dic = xmltodict.parse(f.read())
+
+    dp = dic["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"]
+    y = dp["intensities"]["#text"]
+    y = list(map(float, y.split()))
+
+    if type(dp["positions"]) == list:
+        sp = float(dp["positions"][0]["startPosition"])
+        ep = float(dp["positions"][0]["endPosition"])
+    else:
+        sp = float(dp["positions"]["startPosition"])
+        ep = float(dp["positions"]["endPosition"])
+
+    x = np.arange(sp, ep, (ep - sp) / len(y))
+
+    return x, y
+
+
 # Parse information for the node with type "model"
-# models_info = []
-# for m in models:
-#     model = db.modelcard.find_one({"_id": ObjectId(m)})
-#     context = model["modelContext"]
-#     descript = model["modelDescription"]
-#
-#     model_info = [context["modelName"],
-#                   context["contributor"]["username"],
-#                   context["modelDate"]["modelYear"]]
-#     model_info = break_list(model_info, descript["inputParameters"])
-#     model_info = break_list(model_info, descript["outputParameters"])
-#     model_info = break_list(model_info, descript["hyperParameters"])
-#
-#     model.append(model_info)
+models_info = []
+for m in models:
+    model = db.modelcard.find_one({"_id": ObjectId(m)})
+    context = model["modelContext"]
+    descript = model["modelDescription"]
+
+    model_info = [context["modelName"],
+                  context["contributor"]["username"],
+                  context["modelDate"]["modelYear"]]
+    model_info = break_list(model_info, descript["inputParameters"])
+    model_info = break_list(model_info, descript["outputParameters"])
+    model_info = break_list(model_info, descript["hyperParameters"])
+
+    models_info.append(model_info)
 
 
 # Parse information for the node with type "data"
@@ -83,8 +106,16 @@ for d in datas:
     data = db.datacard.find_one({"_id": ObjectId(d)})
 
     context = data["dataContext"]
+    location = "../" + context["dataLocation"]
+    x, y = detect_x_y(location)
+    minx = str(min(x))
+    maxx = str(max(x))
+    miny = str(min(y))
+    maxy = str(max(y))
+    meany = str(mean(y))
     data_info = [context["center"]["centerName"],
-                 context["contributor"]["username"]]
+                 context["contributor"]["username"],
+                 minx, maxx, miny, maxy, meany]
     sampleID = data["dataContent"]["sampleID"]
     sample = db.sample.find_one({"_id": sampleID})
     data_info.append(sample["sampleName"])
@@ -92,20 +123,28 @@ for d in datas:
     datas_info.append(data_info)
 
 # Training Word2Vec model
-w2v_model = Word2Vec(datas_info, vector_size=256, min_count=1, window=3, sg=1, workers=4)
-w2v_model.save('./w2v_model')
+w2v_model_datas = Word2Vec(datas_info, vector_size=256, min_count=1, window=3, sg=1, workers=4)
+w2v_model_datas.save('./w2v_model_datas')
+w2v_model_models = Word2Vec(models_info, vector_size=256, min_count=1, window=3, sg=1, workers=4)
+w2v_model_models.save('./w2v_model_models')
 
 # Node: Encode model as 0, data as 1
-n = 0
+
 with open('txt/node.txt', 'a+') as f:
+    n = 0
     for m in models:
         model = db.modelcard.find_one({"_id": ObjectId(m)})
         script = "../" + model["modelContext"]["modelLocation"]
 
-        f.write(m + '\t' + str(0) + '\t' + str(ast_to_vec(script)) + '\n')
+        ast_vec = np.array(ast_to_vec(script))
+        info_vec = np.array(get_vec_mean(models_info[n], 256, w2v_model_models))
 
+        f.write(m + '\t' + str(0) + '\t' + str((ast_vec + info_vec) / 2) + '\n')
+        n += 1
+
+    n = 0
     for d in datas:
-        f.write(d + '\t' + str(1) + '\t' + str(get_vec_mean(datas_info[n], 256)) + '\n')
+        f.write(d + '\t' + str(1) + '\t' + str(get_vec_mean(datas_info[n], 256, w2v_model_datas)) + '\n')
         n += 1
 
 # Test: (model, data) [runningTimes, f1_score, precision, recall, cosine similarity, jaccard similarity]
